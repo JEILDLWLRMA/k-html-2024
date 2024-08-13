e2e389f763090041ea0594ebd32c9774f779e06d
ENvironmentSet <sbshw1@gmail.com>
2024-08-13 23:44:58 +0900

    Implement chat mockup

--- src/Counsel.jsx
+++ src/Counsel.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { Link } from "wouter";
 import { css } from "@emotion/react";
-import { accent, gray2, gray1, gray5 } from "./colors.js";
+import { accent, gray2, gray1, white } from "./colors.js";
 
 import menu from "./assets/menu.png";
 import menu2 from "./assets/menu2.png";
@@ -9,6 +9,8 @@ import home from "./assets/home.png";
 import newWrite from "./assets/newWrite.png";
 import pencil from "./assets/pencil.png";
 import trash from "./assets/trash.png";
+import arrowUp from './assets/arrow-up.png'
+import { Message } from './Message.jsx'
 
 const HeaderStyle = {
   header: css`
@@ -41,16 +43,17 @@ const SideBarStyle = {
   home: css`
     width: 1.5rem;
     position: absolute;
-    top: 0.5rem;
+    top: 0.6rem;
     right: 1rem;
   `,
   left: css`
     display: flex;
-    position: relative;
+    position: absolute;
     flex-direction: column;
     background-color: ${gray1};
-    flex-grow: 1;
     width: 50%;
+    height: 100%;
+    z-index: 1;
   `,
   title: css`
     font-size: 1.7rem;
@@ -183,7 +186,125 @@ function Header({ className, handleClickMenu }) {
   );
 }
 
+const FlexibleInputStyle = {
+  box: css`
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    
+    border: 2px solid ${white};
+    border-radius: 90px;
+    
+    padding: 0.8rem 1.2rem;
+    
+    opacity: 0.5;
+    --form-opacity: 0.5;
+    
+    :valid {
+      opacity: 1;
+      --form-opacity: 1;
+    }
+  `,
+  inputField: css`
+    max-width: 90%;
+    
+    background-color: transparent;
+    border: 0;
+    outline: none;
+    
+    color: ${white};
+    font-height: 1.4rem;
+  
+    flex-grow: 1;
+  `,
+  submit: css`
+    height: 2rem;
+    width: 2rem;
+  
+    background-color: ${accent};
+    
+    border: 0;
+    border-radius: 45px;
+    
+    opacity: var(--form-opacity);
+    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: contain;
+    }
+    
+    &[data-parent-is-valid="true"] {
+      opacity: 1;
+    }
+  `
+}
 
+function FlexibleInput({ onSubmit, className }) {
+  return (
+    <form
+      css={FlexibleInputStyle.box}
+      className={className}
+      onSubmit={e => {
+        e.preventDefault()
+        onSubmit(e)
+      }}
+    >
+      <input required css={FlexibleInputStyle.inputField} />
+      <button css={FlexibleInputStyle.submit}>
+        <img src={arrowUp} />
+      </button>
+    </form>
+  )
+}
+
+const UserMessageStyle = css`
+  display: flex;
+  flex-direction: row-reverse;
+  
+  padding-right: 1rem;
+`
+
+function UserMessage({ children, className }) {
+  return (
+    <section css={UserMessageStyle} className={className}>
+      {children}
+    </section>
+  )
+}
+
+const CounselStyle = {
+  main: css`
+    flex-grow: 1;
+    
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    
+    max-height: 92%;
+  `,
+  header: css`
+    height: 8%;
+  `,
+  input: css`
+    margin: 0 0.6rem 1.2rem 0.6rem;
+  `,
+  messageList: css`   
+    flex-grow: 1;
+  
+    padding: 1.8rem 0.8rem;
+    
+    display: flex;
+    flex-direction: column;
+    gap: 1.8rem;
+    
+    overflow-y: scroll;
+  `
+}
 
 export function Counsel() {
   const [sideBar, setSideBar] = useState(false);
@@ -201,8 +322,20 @@ export function Counsel() {
         flex-direction: column;
       `}
     >
-      <Header handleClickMenu={handleClickMenu} />
-      {sideBar ? <SlideBar MenuBox={MenuBox} /> : <></>}
+      <Header handleClickMenu={handleClickMenu} css={CounselStyle.header} />
+      <main css={CounselStyle.main}>
+        {sideBar && <SlideBar MenuBox={MenuBox} />}
+        <div css={CounselStyle.messageList}>
+          <Message><p>안녕하세요!</p></Message>
+          <UserMessage css={css`width: 70%; align-self: end;`}><p>안녕하세용</p></UserMessage>
+          <Message><p>조금 긴 메시지입니다.</p></Message>
+          <UserMessage css={css`width: 70%; align-self: end;`}><p>저도 긴 메시지입니다.</p></UserMessage>
+          <Message><p>아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다.</p></Message>
+          <Message><p>아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다.</p></Message>
+          <Message><p>아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다. 아주 긴 메시지입니다.</p></Message>
+        </div>
+        <FlexibleInput css={CounselStyle.input} />
+      </main>
     </div>
   );
 }
---
+++ src/Message.jsx
@@ -0,0 +1,24 @@
+import { css } from '@emotion/react'
+
+import { MascotAvatar } from './MascotAvatar.jsx'
+
+const MessageStyle = {
+  message: css`
+    display: grid;
+    grid-template-columns: 2.8rem 60%;
+    align-items: center;
+    gap: 1.5rem;
+  `,
+  sender: css`    
+    align-self: start;
+  `
+}
+
+export function Message({ children }) {
+  return (
+    <section css={MessageStyle.message}>
+      <MascotAvatar css={MessageStyle.sender} />
+      {children}
+    </section>
+  )
+}
