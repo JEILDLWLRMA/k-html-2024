d1ddb0e61b472b3ff5a5b04b244a4087ac7895fb
ENvironmentSet <sbshw1@gmail.com>
2024-08-13 09:55:02 +0900

    refactor style definition

--- src/MascotAvatar.jsx
+++ src/MascotAvatar.jsx
@@ -3,13 +3,15 @@ import { css } from '@emotion/react'
 import mascot from './assets/mascot.png'
 import { accent } from './colors.js'
 
-const MascotAvatarStyle = css`
-  border: 3px solid ${accent};
-  border-radius: 50%;
+const MascotAvatarStyle = {
+  img: css`
+    border: 3px solid ${accent};
+    border-radius: 50%;
   
-  aspect-ratio: 1/1;
-  object-fit: cover;
-`
+    aspect-ratio: 1/1;
+    object-fit: cover;
+  `
+}
 
 export function MascotAvatar({ className }) {
   return <img src={mascot} css={MascotAvatarStyle} className={className} />
