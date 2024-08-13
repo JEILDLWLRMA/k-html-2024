c20b80c0a8ae961d827aa8db08662b15b5392674
ENvironmentSet <sbshw1@gmail.com>
2024-08-14 01:18:02 +0900

    add current chat info

--- src/state.js
+++ src/state.js
@@ -2,6 +2,7 @@ import { createContext, useContext } from 'react'
 
 const persistedState = localStorage.getItem('khtml/global')
 export const stateDefault = {
+  currentChat: null,
   chats: [], // list of chat ids
   preference: null, // name of the job
   likes: [], // list of post ids liked
